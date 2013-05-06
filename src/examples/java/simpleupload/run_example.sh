bf6e6b795431c532d70bede3d1776ef429b6554f
Norman Maurer <nmaurer@redhat.com>
2013-05-06 21:44:37 +0200

    Remove local changes

--- src/examples/java/simpleupload/run_example.sh
+++ src/examples/java/simpleupload/run_example.sh
@@ -1,5 +1,4 @@
 # We set VERTX_MODS to point to where we built the module
-#export VERTX_MODS=$(readlink -f '../../../../mods')
-export VERTX_MODS=/Users/norman/Documents/workspace-intellij/mod-formupload/mods
+export VERTX_MODS=$(readlink -f '../../../../mods')
 # And then we run the example
 vertx run SimpleUploadServer.java -includes io.vertx~mod-formupload~2.0.0-SNAPSHOT
